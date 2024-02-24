48c04260bec2a7d0dd2a272800451c59fa460d52
Ahmed Al-Khalidi <96474575+Ahmed-Al-Khalidi@users.noreply.github.com>
2024-02-24 23:38:03 +0300

    Update Image_processing.py

--- Image_processing.py
+++ Image_processing.py
@@ -97,5 +97,41 @@ def RGB_to_grayscale(image_array):
 
 
 
+# Function 5 Pipeline with image resizing feature
+
+def image_pipeline_with_resize(folder_path,height,width):
+    images_data = []
+    labels = []
+    size=(height,width)
+    Classes = os.listdir(folder_path)
+
+    for i in range(len(Classes)):
+        new_path = folder_path + '/' + Classes[i]
+        Classes2 = os.listdir(new_path)
+
+        for j in range(len(Classes2)):
+            labels.append(Classes[i])
+
+            image_path = folder_path + '/' + Classes[i] + '/' + Classes2[j]
+            
+            try:
+                # Attempt to open the image
+                image = Image.open(image_path)
+
+                # Resize the images
+                img_resized = image.resize(size, Image.Resampling.LANCZOS)
+                
+                # Convert the image to a NumPy array
+                image_array = np.array(img_resized)
+                
+                # Append the image array to the list
+                images_data.append(image_array)
+            
+            except (IOError, OSError) as e:
+                # Handle the exception (e.g., print a warning, skip the image, etc.)
+                print(f"Error processing {image_path}: {e}")
+
+    return images_data, labels
+
 
 

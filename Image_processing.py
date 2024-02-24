fd46fcd47c1f59497dbbf99228add895d6840e64
Ahmed Al-Khalidi <96474575+Ahmed-Al-Khalidi@users.noreply.github.com>
2024-02-24 20:58:27 +0300

    Update Image_processing.py
    I added function 2

--- Image_processing.py
+++ Image_processing.py
@@ -4,7 +4,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-
+# Function 1 images pipeline loader
 def image_pipeline(folder_path):
     images_data = []
     labels = []
@@ -35,5 +35,33 @@ def image_pipeline(folder_path):
 
     return images_data, labels
 
-####
+# Function 2 Divide the dataset to train, validation and test datasets
+def Dataset_division(features,labels,val_per,test_per):
+    # Create lists for training, validation data and lists for test data
+    train_features=[]
+    train_labels=[]
+    #################
+    val_features=[]
+    val_labels=[]
+    ################
+    test_features=[]
+    test_labels=[]
+    # Calculate the number of validation data and test data from the original data
+    val_len=int(val_per*len(labels))
+    test_len=int(test_per*len(labels))
+    train_len=((len(labels)-(val_len+test_len)))
+    # Start dividing the data
+    for i1 in range(train_len):
+        train_features.append(features[i1])
+        train_labels.append(labels[i1])
+
+    for i2 in range(val_len):
+        val_features.append(features[(i2+train_len)])
+        val_labels.append(labels[i2+train_len])
+
+    for i3 in range(test_len):
+        test_features.append(features[i3+train_len+val_len])
+        test_labels.append(labels[i3+train_len+val_len])
+
+    return train_features,train_labels,val_features,val_labels,test_features,test_labels
 
